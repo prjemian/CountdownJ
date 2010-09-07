fe35ca7c4d4cdf2196826199e88222272d360e48
jemian <jemian@4ca16b70-4e1b-0410-88e2-31485261bf7f>
2010-09-07 02:24:09 +0000

    change license information in each file, per GPLv3
    git-svn-id: http://svn.jemian.org/svn/regitte/CountdownJ@1376 4ca16b70-4e1b-0410-88e2-31485261bf7f

--- src/org/jemian/countdownj/Swing/GuiSwing.java
+++ src/org/jemian/countdownj/Swing/GuiSwing.java
@@ -11,7 +11,23 @@
 
 package org.jemian.countdownj.Swing;
 
-// CountdownJ, (c) 2010 Pete R. Jemian, See LICENSE (GPLv3) for details
+/*
+    CountdownJ, (c) 2010 Pete R. Jemian <prjemian@gmail.com>
+    See LICENSE (GPLv3) for details.
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 //########### SVN repository information ###################
 //# $Date$
